24da5d260f4dec16efe19270512b999a408c4828
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-02-01 14:26:11 +0100

    Calculator changes

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -10,24 +10,39 @@ namespace Calculator
     {
         public double Add(double a, double b)
         {
-            return a + b;
+            var result = a + b;
+            Accumulator = result;
+            return result;
+
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            var result = a - b;
+            Accumulator = result;
+            return result;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            var result = a * b;
+            Accumulator = result;
+            return result;
         }
 
         public double Power(double a, double b)
         {
-            return Math.Pow(a, b);
+            var result = Math.Pow(a, b);
+            Accumulator = result;
+            return result;
         }
 
+        public double Accumulator { get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
 
     }
 }
